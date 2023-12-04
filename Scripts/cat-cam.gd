284f3a7dc748fa69c9a1ac1a2e3b2ac80221a532
Brandon W <brandon.a.wheatley@gmail.com>
2023-12-04 09:15:05 -0600

    revamped catcam

--- Scripts/cat-cam.gd
+++ Scripts/cat-cam.gd
@@ -1,26 +1,23 @@
 extends AnimatedSprite2D
 
-
-var changing_portrait : bool = false  # Flag to track if portrait change is in progress
-var original_position : Vector2  # Variable to store the original position
-@onready var _animated_sprite = $AnimatedSprite2D
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass	
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 	
-func _on_cat_launched():
-	print("launched")
-	_animated_sprite.play("launched")
-		
-func _on_cat_landed():
-	print("landed")
-	_animated_sprite.play("landed")
+func _on_player_cat_landed():
+	var total_frames = sprite_frames.get_frame_count("landed")
+	var random_frame = randi() % total_frames
+	frame = random_frame
 	
-func _on_player_aiming():
-	print("player_aiming")
-	_animated_sprite.play("player_aiming")
+func _on_player_cat_launched():
+	var total_frames = sprite_frames.get_frame_count("launched")
+	var random_frame = randi() % total_frames
+	frame = random_frame
+	
+func _on_aim_lines_player_aiming():
+	var total_frames = sprite_frames.get_frame_count("player_aiming")
+	var random_frame = randi() % total_frames
+	frame = random_frame
