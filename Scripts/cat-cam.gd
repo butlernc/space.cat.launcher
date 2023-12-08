9b87614a2d51e2938685ddca3acc493d5c55f1a7
Brandon W <brandon.a.wheatley@gmail.com>
2023-12-07 18:32:34 -0600

    revamped catcam

--- Scripts/cat-cam.gd
+++ Scripts/cat-cam.gd
@@ -1,6 +1,7 @@
 extends AnimatedSprite2D
 var is_first_time_aiming_on_planet = false
 
+
 func _ready():
 	pass
 
@@ -8,6 +9,9 @@ func _ready():
 func _process(delta):
 	pass
 	
+func _on_scene_start():
+	random_frame("default")
+
 func _on_player_cat_landed():
 	is_first_time_aiming_on_planet = true
 	random_frame("landed")
@@ -15,14 +19,19 @@ func _on_player_cat_landed():
 func _on_player_cat_launched():
 	random_frame("launched")
 	
-	
 func _on_aim_lines_player_aiming():
 	if(is_first_time_aiming_on_planet):
 		random_frame("player_aiming")
 		is_first_time_aiming_on_planet = false
-
+		
+func _on_reset_pressed():
+	if randf() < 0.2:
+		random_frame("special")
+	else:
+		random_frame("default")
 
 func random_frame(context):
 	var total_frames = sprite_frames.get_frame_count(context)
 	var random_frame = randi() % total_frames
-	frame = random_frame
+	animation = context
+	set_frame(random_frame)
